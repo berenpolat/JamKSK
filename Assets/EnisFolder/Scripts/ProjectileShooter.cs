08cd61c52a98abb5189eb95f2d1de33c6b67224f
Enis Ay <enisay35@outlook.com>
2025-04-26 10:14:35 +0300

    ...

--- Assets/EnisFolder/Scripts/ProjectileShooter.cs
+++ Assets/EnisFolder/Scripts/ProjectileShooter.cs
@@ -1,19 +1,18 @@
 using System;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class ProjectileShooter3D : MonoBehaviour
 {
-    public static ProjectileShooter3D Instance { get; set;}
+    public static ProjectileShooter3D Instance { get; set; }
+
     public GameObject projectilePrefab;
     public Transform firePoint;
     public float projectileSpeed = 15f;
     public float firePointRadius = 1.5f;
 
     public bool isProjectileArmed;
-
     public bool isSwordArmed;
 
     private Camera cam;
@@ -23,9 +22,18 @@ public class ProjectileShooter3D : MonoBehaviour
     [SerializeField] private GameObject swordPowerUp;
     public GameObject swordObject;
 
-    
     private GameObject currentPowerUpInstance;
     private GameObject currentSwordPowerUp;
+
+    // UI ve LevelEnder için
+    public GameObject holdToInteractUI;
+    public Image holdToInteractBar;
+
+    private bool isNearLevelEnder = false;
+    private float holdTime = 0f;
+    public float requiredHoldTime = 2f;
+    private GameObject currentLevelEnder;
+
     private void Awake()
     {
         Instance = this;
@@ -37,7 +45,7 @@ public class ProjectileShooter3D : MonoBehaviour
         isProjectileArmed = false;
         isSwordArmed = false;
         swordObject.SetActive(false);
-        
+        holdToInteractUI.SetActive(false);
     }
 
     void Update()
@@ -50,29 +58,53 @@ public class ProjectileShooter3D : MonoBehaviour
         }
 
         if (Input.GetKeyDown(KeyCode.Mouse1) && isSwordArmed)
-        { 
-            Sword();   
+        {
+            Sword();
+        }
+
+        if (isNearLevelEnder)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                holdTime += Time.deltaTime;
+            
+                float targetFillAmount = holdTime / requiredHoldTime;
+                holdToInteractBar.fillAmount = Mathf.Lerp(holdToInteractBar.fillAmount, targetFillAmount, Time.deltaTime * 10f);
+
+                if (holdTime >= requiredHoldTime)
+                {
+                    if (currentLevelEnder != null)
+                    {
+                        Destroy(currentLevelEnder);
+                    }
+
+                    holdToInteractUI.SetActive(false);
+                    isNearLevelEnder = false;
+                    holdTime = 0f;
+                    holdToInteractBar.fillAmount = 0f;
+                }
+            }
+            else if (Input.GetKeyUp(KeyCode.E))
+            {
+                holdTime = 0f;
+                holdToInteractBar.fillAmount = 0f;
+            }
         }
     }
+
 
     void FollowMouseOnXYPlane()
     {
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
-        // Karakterin bulunduğu XY düzlemine ray gönder
         Plane plane = new Plane(Vector3.forward, transform.position);
 
         if (plane.Raycast(ray, out float distance))
         {
             Vector3 mouseWorldPos = ray.GetPoint(distance);
-
             Vector3 direction = (mouseWorldPos - transform.position).normalized;
 
-            // FirePoint karakterin etrafında dairede döner
             Vector3 offset = direction * firePointRadius;
             firePoint.position = transform.position + offset;
-
-            // FirePoint mouse yönüne dönsün
             firePoint.rotation = Quaternion.LookRotation(Vector3.forward, direction);
         }
     }
@@ -80,11 +112,10 @@ public class ProjectileShooter3D : MonoBehaviour
     void Shoot()
     {
         GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
-
         Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
         if (rb != null)
         {
-            // FirePoint'in yukarı yönüne doğru fırlat (LookRotation bu yöne bakıyor)
             rb.velocity = firePoint.up * projectileSpeed;
             isProjectileArmed = false;
             Destroy(currentPowerUpInstance);
@@ -97,35 +128,55 @@ public class ProjectileShooter3D : MonoBehaviour
         swordObject.SetActive(true);
         animator.SetTrigger("sword");
     }
+
     public void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag == "projectiler" && isProjectileArmed == false)
+        if (other.CompareTag("projectiler") && !isProjectileArmed)
         {
             isProjectileArmed = true;
-
-            // İstenilen objeyi spawn et
             currentPowerUpInstance = Instantiate(projectilePowerUp, transform.position, Quaternion.identity);
-
             Destroy(other.gameObject);
         }
 
-        if (other.transform.tag == "sworder")
+        if (other.CompareTag("sworder"))
         {
             isSwordArmed = true;
-
             currentSwordPowerUp = Instantiate(swordPowerUp, transform.position, quaternion.identity);
             Destroy(other.gameObject);
         }
 
-
-        if (other.transform.tag == "Ball")
+        if (other.CompareTag("Ball") && !PlayerController.Instance.isDashing)
         {
             Destroy(gameObject);
         }
 
-        if (other.gameObject.tag == "Enemy" && !PlayerController.Instance.isDashing)
+        if (other.CompareTag("Enemy") && !PlayerController.Instance.isDashing)
         {
             Destroy(gameObject);
+        }
+
+        if (other.CompareTag("trap"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (other.CompareTag("levelEnder"))
+        {
+            isNearLevelEnder = true;
+            holdToInteractUI.SetActive(true);
+            currentLevelEnder = other.gameObject;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("levelEnder"))
+        {
+            isNearLevelEnder = false;
+            holdToInteractUI.SetActive(false);
+            holdTime = 0f;
+            holdToInteractBar.fillAmount = 0f;
+            currentLevelEnder = null;
         }
     }
 }
