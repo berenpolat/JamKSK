3b0feb57102c879cacd1aa120d3410254d4b24f7
berenelcn <polatberen24@gmail.com>
2025-04-27 00:23:07 +0300

    sdzfhjx

--- Assets/ButtonController.cs
+++ Assets/ButtonController.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class ButtonController : MonoBehaviour
 {
     public GameObject mainMenuPanel;
+    public GameObject levelFailPanel;
+    public GameObject completePanel;
     public void PlayButton()
     {
         mainMenuPanel.SetActive(false);
+    }
+
+    public void PlayLevelAgainButton()
+    {
+        levelFailPanel.SetActive(false);
+        LevelManager.Instance.RestartLevel();
+    }
+
+    public void NextLevelButton()
+    {
+        levelFailPanel.SetActive(false);
         LevelManager.Instance.LoadNextLevel();
     }
-    
+    public void BackToMenuButton()
+    {
+        completePanel.SetActive(false);
+        LevelManager.Instance.LoadNextLevel();
+    }
 }
