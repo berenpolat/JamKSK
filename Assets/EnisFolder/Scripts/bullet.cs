78e62a4e7450825f343673dd793cb966a7a0649d
berenelcn <polatberen24@gmail.com>
2025-04-27 13:19:33 +0300

    wetsd

--- Assets/EnisFolder/Scripts/bullet.cs
+++ Assets/EnisFolder/Scripts/bullet.cs
@@ -3,12 +3,20 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
     [SerializeField] private CameraFollow cameraFollow; // CameraFollow scripti
+    [SerializeField] private bool isSword ;
 
     void Start()
     {
+        isSword = false;
         if (cameraFollow == null)
         {
             cameraFollow = FindObjectOfType<CameraFollow>();
+        }
+
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter != null && meshFilter.name == "PlayerSword")
+        {
+            isSword = true;
         }
     }
 
@@ -16,7 +24,11 @@ public class bullet : MonoBehaviour
     {
         if (other.CompareTag("Enemy"))
         {
-            Destroy(gameObject);
+            if (!isSword)
+            {
+                Destroy(gameObject);    
+            }
+            
             cameraFollow.ShakeCamera(0.01f, 0.3f, vibrato: 5, randomness: 10f);
             // Shake tetikle
         }
