2e1dddac136756ab4391eb5bfffaed19bcf0b74e
Enis Ay <enisay35@outlook.com>
2025-04-27 03:46:45 +0300

    ...

---
+++ Assets/EnisFolder/Scripts/bullet.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
